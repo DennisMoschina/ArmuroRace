bbdcb91a999dd949368ce8414f4daf68a3dfbb99
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2023-07-24 11:51:34 +0200

    added documentation to all methods

--- lib/blinkLED/blinkLED.h
+++ lib/blinkLED/blinkLED.h
@@ -3,9 +3,24 @@
 
 #include "armuro.h"
 
+/**
+ * @brief Blink the LED on the given side with the given time interval
+ * 
+ * @param side the side to blink the LED on
+ * @param timeInterval the interval at which the LED should blink
+ */
 void blinkLED(Side side, uint16_t timeInterval);
+
+/**
+ * @brief Stop blinking the LED and turn it off on the given side
+ * 
+ * @param side the side to of the LED
+ */
 void stopBlinkingLED(Side side);
 
+/**
+ * @brief Run the blinking Task
+ */
 void blinkLEDTask();
 
 #endif
